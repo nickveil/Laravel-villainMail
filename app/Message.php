6c5e0fcfa67fc5a92e0f0084d484415d1a42a361
nickveil <31191433+nickveil@users.noreply.github.com>
2017-11-09 11:25:37 -0500

    fixed bug: date displaying on home screen has new format

--- app/Message.php
+++ app/Message.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use Carbon\Carbon;
+
 class Message extends Model
 {
     
@@ -14,5 +16,19 @@ class Message extends Model
   public function recipient() {
     return $this->belongsTo('App\User', 'recipient_id');
   }
+
+  public function getCreatedAtAttribute($date){
+
+ 		return Carbon::createFromFormat('Y-m-d H:i:s', $date)->toDayDateTimeString();
+  }
+
+  // Stumbled upon this while changing date formate might be good to
+  // implement last updated section but not useful right now.
+
+  // public function getUpdatedAtAttribute($date){
+
+ 	// 	return Carbon::createFromFormat('Y-m-d H:i:s', $date)->toFormattedDateString();
+  // }
+ 
 
 }
